95b6570ac59ab10046af3fa320154b7ae8f6a097
Aidar Khabirov <khabirov.ar1990@gmail.com>
2026-01-19 17:57:55 +0500

    Build static site

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -173,7 +173,7 @@ def extract_title(markdown):
             return title
     raise Exception("No title in markdown file")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath='/'):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
     with open(from_path, 'r') as f:
         markdown = f.read()
@@ -183,19 +183,21 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     page = template.replace('{{ Title }}', title)
     page = page.replace('{{ Content }}', content)
+    page = page.replace('href="/', f'href="{basepath}')
+    page = page.replace('src="/', f'src="{basepath}')
     dirs = os.path.dirname(dest_path)
     if dirs:
         os.makedirs(dirs, exist_ok = True)
     with open(dest_path, 'w') as f:
         f.write(page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath='/'):
     for file in os.listdir(dir_path_content):
         print(F"Debug: dir_path_content={dir_path_content}, current file={file}")
         from_path = os.path.join(dir_path_content, file)
         dest_path = os.path.join(dest_dir_path, file)
         dest_path = dest_path.replace('.md', '.html')
         if os.path.isfile(from_path): #and os.path.splitext(from_path) == '.md':
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
+import sys
 from textnode import TextNode
 from htmlnode import copy_static_to_public, generate_page, generate_pages_recursive
 
-def main():
-    copy_static_to_public('static', 'public')
-    # generate_page('content/index.md', 'template.html', 'public/index.html')
-    generate_pages_recursive('content', 'template.html', 'public')
 
+
+def main():
+    copy_static_to_public('static', 'doc')
+    # generate_page('content/index.md', 'template.html', 'public/index.html')
+    print(f"Debug: SYS_ARGV={sys.argv}")
+    basepath = '/'
+    if len(sys.argv) >= 2:
+        basepath = sys.argv[1]
+    print(f"Debug: basepath={basepath}")
+    generate_pages_recursive('content', 'template.html', 'doc', basepath)
+    
 
 if __name__ == '__main__':
     main()
