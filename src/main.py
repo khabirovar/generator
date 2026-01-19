d9ffa4330e5c578cb1ac0ec8b54a239f37ef9ba8
Aidar Khabirov <khabirov.ar1990@gmail.com>
2026-01-19 18:09:43 +0500

    Build static site again

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ from htmlnode import copy_static_to_public, generate_page, generate_pages_recurs
 
 
 def main():
-    copy_static_to_public('static', 'doc')
+    copy_static_to_public('static', 'docs')
     # generate_page('content/index.md', 'template.html', 'public/index.html')
     print(f"Debug: SYS_ARGV={sys.argv}")
     basepath = '/'
     if len(sys.argv) >= 2:
         basepath = sys.argv[1]
     print(f"Debug: basepath={basepath}")
-    generate_pages_recursive('content', 'template.html', 'doc', basepath)
+    generate_pages_recursive('content', 'template.html', 'docs', basepath)
     
 
 if __name__ == '__main__':
